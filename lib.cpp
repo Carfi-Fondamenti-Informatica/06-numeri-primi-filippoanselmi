d9cabd35706f05f226044c7028b8e42b772305ee
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-11-30 12:36:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+int primo(int n, int m) {
+  bool x=true;
+  
+  if (m!=1) {
+    if ((n % m) != 0) {
+      m--;
+      x=primo(n, m);
+    } else {
+      x=false;
+    }
+  } else {
+    x=true;
+  }
+  return x;
+}
