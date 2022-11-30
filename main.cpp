b7391491464d7164be75f73dac35b13b980e138a
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-11-30 12:40:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-int n=0; cin >> n;
-int m=(n-1);
+  int n=0; cin >> n;
+  int m=(n-1);
 
-if (primo(n,m) == true) {
-cout << "numero primo";
-} else if (primo(n,m) == false) {
-cout << "numero non primo";
-}
+  if (primo(n,m) == true) {
+    cout << "numero primo";
+  } else if (primo(n,m) == false) {
+    cout << "numero non primo";
+  }
   return 0;
 }
